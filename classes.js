1e079f6c859535bbc28da54f1404507f423874bb
Maria F. Matsunaga <192991413+Matsunanda@users.noreply.github.com>
2026-03-05 15:51:47 -0300

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,5 @@
+const API_URL = 'https://crudcrud.com/api/f19db97cad464ee3b32281cb540cd747/clientes';
+const form = document.getElementById('formclientes');
+const inputNome = document.getElementById('nome');
+const inputEmail = document.getElementById('email');
+const listaDiv = document.getElementById('listaclientes');
